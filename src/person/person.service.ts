4fc4ece14e598889a11809eb3ec98ebb1c3262c5
wb-mty940435 <wb-mty940435@cainiao.com>
2024-07-19 17:20:18 +0800

    init

---
+++ src/person/person.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { CreatePersonDto } from './dto/create-person.dto';
+import { UpdatePersonDto } from './dto/update-person.dto';
+
+@Injectable()
+export class PersonService {
+  create(createPersonDto: CreatePersonDto) {
+    console.log('CreatePersonDto', createPersonDto);
+    return 'This action adds a new person' + JSON.stringify(createPersonDto);
+  }
+
+  findAll() {
+    return `This action returns all person`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} person`;
+  }
+
+  update(id: number, updatePersonDto: UpdatePersonDto) {
+    return `This action updates a #${id} person`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} person`;
+  }
+}
